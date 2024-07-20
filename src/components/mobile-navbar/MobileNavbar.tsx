c88a33161926aaeee89ec5395f16b8368427b4e4
Yash Prajapati <yp54139@gmail.com>
2024-07-20 14:25:00 +0530

    ui-fix

--- src/components/mobile-navbar/MobileNavbar.tsx
+++ src/components/mobile-navbar/MobileNavbar.tsx
@@ -12,6 +12,7 @@ import {
   offer_white,
 } from "../../assets";
 import { useLocation, useNavigate } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface MenuItem {
   title: string;
@@ -103,112 +104,122 @@ const MobileNavbar = ({ transparentBg = false }) => {
     ];
 
     return (
-      <div className="fixed top-0 left-0 w-screen h-full bg-white z-[999999] overflow-y-auto p-6">
-        <button
-          onClick={() => setIsMobileMenuOpen(false)}
-          className="absolute top-1 right-2 text-black text-2xl"
-        >
-          &times;
-        </button>
-
-        <div className="flex flex-col space-y-4">
-          <div
-            className="flex items-center bg-[#AB0101] bg-opacity-5 rounded-lg p-3"
-            onClick={() => {
-              navigate("/login");
-              setIsMobileMenuOpen(false);
-            }}
+      <AnimatePresence>
+        {isMobileMenuOpen && (
+          <motion.div
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ type: "tween", duration: 0.3 }}
+            className="fixed top-0 left-0 w-screen h-full bg-white z-[999999] overflow-y-auto p-6"
           >
-            <div className="bg-black rounded-full p-2 mr-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                />
-              </svg>
-            </div>
-            <div>
-              <h3 className="font-semibold">Login or Create Account</h3>
-              <p className="text-sm text-gray-600">
-                Manage your profile, traveller details, login details
-              </p>
-            </div>
-          </div>
+            <button
+              onClick={() => setIsMobileMenuOpen(false)}
+              className="absolute top-1 right-2 text-black text-2xl"
+            >
+              &times;
+            </button>
 
-          <div className="grid grid-cols-2 gap-4">
-            {navs.map((nav) => (
+            <div className="flex flex-col space-y-4">
               <div
-                key={nav.title}
-                className={`flex flex-col items-center justify-center p-4 rounded-lg ${
-                  location.pathname === `/${nav.link}`
-                    ? "bg-[#AB0101] text-white"
-                    : "bg-black text-white"
-                }`}
+                className="flex items-center bg-[#AB0101] bg-opacity-5 rounded-lg p-3"
                 onClick={() => {
-                  navigate(`/${nav.link}`);
+                  navigate("/login");
                   setIsMobileMenuOpen(false);
                 }}
               >
-                <img
-                  src={
-                    location.pathname === `/${nav.link}`
-                      ? nav.activeIcon
-                      : nav.passiveIcon
-                  }
-                  alt={nav.title}
-                  className="w-6 h-6 mb-2"
-                />
-                <span className="text-sm">{nav.title}</span>
-              </div>
-            ))}
-          </div>
-
-          <div className="space-y-3">
-            {allMenuItems.map((item) => (
-              <div
-                key={item.title}
-                className="flex items-center rounded-lg p-3 gap-3 border-[1px] border-[#B9B9B9]"
-                onClick={() => {
-                  if (item.route) {
-                    navigate(item.route);
-                  }
-                  setIsMobileMenuOpen(false);
-                }}
-              >
-                <div className="bg-black rounded-full flex items-center justify-center w-14 h-10">
-                  <img src={item.icon} alt="hash" />
+                <div className="bg-black rounded-full p-2 mr-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                    />
+                  </svg>
                 </div>
                 <div>
-                  <h3 className="font-semibold">{item.title}</h3>
-                  <p className="text-sm text-gray-600">{item.desc}</p>
+                  <h3 className="font-semibold">Login or Create Account</h3>
+                  <p className="text-sm text-gray-600">
+                    Manage your profile, traveller details, login details
+                  </p>
                 </div>
               </div>
-            ))}
-          </div>
 
-          <div className="text-center text-sm text-red-600 mt-4">
-            <a href="#" className="underline">
-              Privacy Policy
-            </a>{" "}
-            ,{" "}
-            <a href="#" className="underline">
-              T&Cs
-            </a>{" "}
-            and{" "}
-            <a href="#" className="underline">
-              User Agreement
-            </a>
-          </div>
-        </div>
-      </div>
+              <div className="grid grid-cols-2 gap-4">
+                {navs.map((nav) => (
+                  <div
+                    key={nav.title}
+                    className={`flex flex-col items-center justify-center p-4 rounded-lg ${
+                      location.pathname === `/${nav.link}`
+                        ? "bg-[#AB0101] text-white"
+                        : "bg-black text-white"
+                    }`}
+                    onClick={() => {
+                      navigate(`/${nav.link}`);
+                      setIsMobileMenuOpen(false);
+                    }}
+                  >
+                    <img
+                      src={
+                        location.pathname === `/${nav.link}`
+                          ? nav.activeIcon
+                          : nav.passiveIcon
+                      }
+                      alt={nav.title}
+                      className="w-6 h-6 mb-2"
+                    />
+                    <span className="text-sm">{nav.title}</span>
+                  </div>
+                ))}
+              </div>
+
+              <div className="space-y-3">
+                {allMenuItems.map((item) => (
+                  <div
+                    key={item.title}
+                    className="flex items-center rounded-lg p-3 gap-3 border-[1px] border-[#B9B9B9]"
+                    onClick={() => {
+                      if (item.route) {
+                        navigate(item.route);
+                      }
+                      setIsMobileMenuOpen(false);
+                    }}
+                  >
+                    <div className="bg-black rounded-full flex items-center justify-center w-14 h-10">
+                      <img src={item.icon} alt="hash" />
+                    </div>
+                    <div>
+                      <h3 className="font-semibold">{item.title}</h3>
+                      <p className="text-sm text-gray-600">{item.desc}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <div className="text-center text-sm text-red-600 mt-4">
+                <a href="#" className="underline">
+                  Privacy Policy
+                </a>{" "}
+                ,{" "}
+                <a href="#" className="underline">
+                  T&Cs
+                </a>{" "}
+                and{" "}
+                <a href="#" className="underline">
+                  User Agreement
+                </a>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     );
   };
 
